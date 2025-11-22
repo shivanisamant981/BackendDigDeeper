94dbf6e99525b5b2d9db4c3e79c95bd91fe70579
shivani <shivanisamant981@gmail.com>
2025-11-22 23:14:49 +0530

    learned cloudnary concept

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,13 @@
+import {v1 as cloudinary} from "cloudinary"
+import fs from "fs"
+
+
+    // Configuration
+    cloudinary.config({ 
+        cloud_name: 'dnvtcsfhd', 
+        api_key: '956711494411914', 
+        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+    });
+    
+  
+   
