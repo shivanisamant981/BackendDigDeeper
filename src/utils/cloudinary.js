34954bfa99306f009f22dba7af5d1a6ef13de314
shivani <shivanisamant981@gmail.com>
2025-11-24 19:42:08 +0530

    sucessfully written cloudinary code

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -1,13 +1,30 @@
-import {v1 as cloudinary} from "cloudinary"
+import {v2 as cloudinary} from "cloudinary"
 import fs from "fs"
 
 
     // Configuration
     cloudinary.config({ 
-        cloud_name: 'dnvtcsfhd', 
-        api_key: '956711494411914', 
-        api_secret: '<your_api_secret>' // Click 'View API Keys' above to copy your API secret
+        cloud_name: process.env.CLOUDINARY_CLOUD_NAME, 
+        api_key: process.env.CLOUDINARY_API_KEY, 
+        api_secret: process.env.CLOUDINARY_API_SECRET // Click 'View API Keys' above to copy your API secret
     });
     
+const uploadOnCloudinary=async (localFilePath)=>{
+    try{
+        if(!localFilePath)return null
+        const response=await cloudinary.uploader.upload(localFilePath,{
+            resource_type:"auto"
+        })
+        //file had been uploaded sucessfully
+        console.log("file is uploaded on cloudinary",response.url)
+        return response;
+    }catch(error){
+        fs.unlinkSync(localFilePath)//remove the locally saved temporary file as the upload 
+        //operation got failed
+        return null;
+
+    }
+}
   
+export {uploadOnCloudinary}
    
