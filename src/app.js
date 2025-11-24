10da658ab751db9f56c8ffab3ee75359b4ee5663
shivani <shivanisamant981@gmail.com>
2025-11-24 22:22:21 +0530

    sucessfully writter userRoute +checked

--- src/app.js
+++ src/app.js
@@ -9,9 +9,17 @@ app.use(cors({
     origin:process.env.CORS_ORIGIN,
     credentials:true
 }))
-app.json(express.json({limit:"16kb"}));
+app.use(express.json({limit:"16kb"}));
 app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"));
 app.use(cookieParser())
+
+
+//routes
+import userRouter from './routes/user.routes.js'
+
+app.use("/api/v1/users",userRouter)
+
+//https:localhost:8000/api/v1/users/register
 
 export {app}
--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
 //require('dotenv').config({path:'./env'})
 
-
+import {app} from './app.js'
 import connectDB from './db/index.js';
 import dotenv from 'dotenv'
+// import express from 'express'
+
+// const app=express();
 dotenv.config({
     path:'./env'
 });
